5d7bf54646f88d5c25bd8ae4425c2574c920de5c
canerkaseler <caner.kaseler@enqura.com>
2020-12-20 21:56:21 +0300

    Completed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.pink[900],
+      ),
+      backgroundColor: Colors.pink[600],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(36.0),
+          child: FlatButton(
+            onPressed: (){
+             setState(() {
+               ballNumber = Random().nextInt(5) + 1;
+             });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+          ),
+        ),
       ),
     );
+  }
+}
